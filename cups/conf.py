2b9515539ed85467a1761bb8960576220cf7248b
mac/cli <chengcli@umich.edu>
2024-01-04 16:46:00 -0500

    update

--- cups/conf.py
+++ cups/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CUP'
+copyright = '2024, Canoe'
+author = 'Cheng Li'
 
 release = '0.1'
 version = '0.1.0'
